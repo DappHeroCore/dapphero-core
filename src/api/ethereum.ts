6b48d250267a09b95bba6dcf3eb25ac089b431f5
ADE <ade@ADEs-MacBook-Pro.local>
2019-12-29 14:25:11 -0500

    Added types for web3

--- src/api/ethereum.ts
+++ src/api/ethereum.ts
@@ -1,9 +1,5 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(); // Needs more parameters
-
-export const getBalance = async (address: string): Promise<string> => new Promise((resolve, reject) => {
-  web3.eth.getBalance(address)
-    .then((balance) => resolve(balance))
-    .catch((err) => reject(err));
-});
+export const getBalance = (address: string, web3Instance: Web3): Promise<string> => {
+  return web3Instance.eth.getBalance(address)
+};
