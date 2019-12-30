55e0c3faf46a47ac475afbb1e4a9e861d4fdef8b
Dennison Bertram <dennison@dennisonbertram.com>
2019-12-30 15:54:25 -0500

    Reducer renders "ETH" compoments

--- src/modules/eth/EthParent.tsx
+++ src/modules/eth/EthParent.tsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 
-export class EthParent extends Component {
-  state = {
-    state1: 1,
-    state2: 3,
-  }
+interface request {
+  requestString: string[];
+  element: HTMLElement;
+  arg: string;
+  index: number;
+}
 
-  render() {
-    return (
-      <div>
-        Some Text
-      </div>
-    )
-  }
+// our components props accept a number for the initial value
+export const EthParent:FunctionComponent<request> = (request) => {
+  // since we pass a number here, clicks is going to be a number.
+  // setClicks is a function that accepts either a number or a function returning
+  // a number
+  const [clicks, setClicks] = useState(0);
+  return <>
+    <p>Clicks: {clicks}</p>
+    <button onClick={() => setClicks(clicks+1)}>+</button>
+    <button onClick={() => setClicks(clicks-1)}>-</button>
+  </>
 }
