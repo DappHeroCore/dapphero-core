c0d8e91b5ef68a0c2bb324b8679b91c8aa39f4d4
michaelcohen716 <michaelcohen716@gmail.com>
2019-12-31 11:15:34 -0500

    inbetween

--- src/modules/eth/EthParent.tsx
+++ src/modules/eth/EthParent.tsx
@@ -1,30 +1,35 @@
 import React, { FunctionComponent, useState } from "react";
 import { Request } from "../types";
 import { EthStaticView } from "./EthStaticView";
-import { EthereumContextConsumer } from "../../context/ethereum"
+import { EthereumContextConsumer } from "../../context/ethereum";
 
 interface EthParentProps {
   request: Request;
 }
 
-// our components props accept a number for the initial value
 export const EthParent: FunctionComponent<EthParentProps> = ({ request }) => {
-  // since we pass a number here, clicks is going to be a number.
-  // setClicks is a function that accepts either a number or a function returning
-  // a number
-  const reducer = () => {
+  console.log("ETHPARENT REQUEst", request);
+  return (
     <EthereumContextConsumer>
       {({ connected, accounts, injected }) => {
-        switch (request.arg) {
+        switch (request.requestString[2]) {
           case "address":
-            if(connected && accounts.length > 0){
-              return <EthStaticView request={request} />;
+          case "getBalance": {
+            // what will address native tag be??
+            if (connected && accounts.length > 0) {
+              return (
+                <EthStaticView
+                  request={request}
+                  injected={injected}
+                  accounts={accounts}
+                />
+              );
             }
+          }
+          default:
+            return null;
         }
-
       }}
     </EthereumContextConsumer>
-  };
-
-  return <div>eth</div>;
+  );
 };
