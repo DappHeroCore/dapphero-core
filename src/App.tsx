d715dd7f37d9713193e0313c74114e89833e1431
Dennison Bertram <dennisonbertram@Dennisons-MacBook-Pro.local>
2020-01-08 17:42:50 -0500

    Put mock contract data in one place

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { EthereumContextProvider } from './context/ethereum';
-import { HTMLContextProvider, HTMLContextConsumer } from './context/html';
-import reducer from './modules/reducer';
+import React, { useState, useEffect } from 'react'
+import { EthereumContextProvider } from './context/ethereum'
+import { HTMLContextProvider, HTMLContextConsumer } from './context/html'
+import reducer from './modules/reducer'
 
-const App: React.FC = () => {
-  return (
-    <EthereumContextProvider>
-      <HTMLContextProvider>
-        <HTMLContextConsumer>
-          {({ elements, requests }) => {
-            {
-              return requests.map(request => {
-                return reducer(request);
-              });
-            }
-          }}
-        </HTMLContextConsumer>
-      </HTMLContextProvider>
-    </EthereumContextProvider>
-  );
-};
+/*
 
-export default App;
+*/
+const App: React.FC = () => (
+  <EthereumContextProvider>
+    <HTMLContextProvider>
+      <HTMLContextConsumer>
+        {({ elements, requests }) => {
+          {
+            return requests.map((request) => reducer(request))
+          }
+        }}
+      </HTMLContextConsumer>
+    </HTMLContextProvider>
+  </EthereumContextProvider>
+)
+
+export default App
