0ff8f5e0005b5f75909c56278aeee68178d60105
ADE <ecg.drake@gmail.com>
2020-01-09 11:55:30 -0500

    Update eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { EthereumContextProvider } from './context/ethereum';
-import { HTMLContextProvider, HTMLContextConsumer } from './context/html';
-import reducer from './modules/reducer';
+import React from 'react'
+import { EthereumContextProvider } from './context/ethereum'
+import { HTMLContextProvider, HTMLContextConsumer } from './context/html'
+import reducer from './modules/reducer'
 
-const App: React.FC = () => {
-  return (
-    <EthereumContextProvider>
-      <HTMLContextProvider>
-        <HTMLContextConsumer>
-          {({ elements, requests }) => {
-            {
-              return requests.map(request => {
-                return reducer(request);
-              });
-            }
-          }}
-        </HTMLContextConsumer>
-      </HTMLContextProvider>
-    </EthereumContextProvider>
-  );
-};
+const App: React.FC = () => (
+  <EthereumContextProvider>
+    <HTMLContextProvider>
+      <HTMLContextConsumer>
+        {({ requests }) => requests.map((request) => reducer(request))}
+      </HTMLContextConsumer>
+    </HTMLContextProvider>
+  </EthereumContextProvider>
+)
 
-export default App;
+export default App
--- src/modules/reducer.tsx
+++ src/modules/reducer.tsx
@@ -1,34 +1,32 @@
-import * as React from 'react';
-import { Request } from './types';
-import { EthParent } from './eth';
-import { DappHeroConfig } from './types';
+import * as React from 'react'
+import { Request, DappHeroConfig } from './types'
+import { EthParent } from './eth'
 
 const getConfig = (): DappHeroConfig => {
-  let config: DappHeroConfig;
-  const configElement = document.getElementById('dh-config');
-  //TODO Clean this data to prevent injections
-  config = JSON.parse(configElement.textContent);
-  //Hide the Element if not hidden.
+  const configElement = document.getElementById('dh-config')
+  // TODO Clean this data to prevent injections
+  const config: DappHeroConfig = JSON.parse(configElement.textContent)
+  // Hide the Element if not hidden.
   if (configElement.style.display !== 'none') {
-    configElement.style.display = 'none';
+    configElement.style.display = 'none'
   }
-  return config;
-};
+  return config
+}
 
-//This incremeents a key so each element out of reducer has unique Key so react doesn't complain.
-let reactKeyIndex = 0;
+// This incremeents a key so each element out of reducer has unique Key so react doesn't complain.
+let reactKeyIndex = 0
 
 const reducer = (request: Request) => {
   switch (request.arg) {
-    case 'eth':
-      const config = getConfig();
-      reactKeyIndex++;
-      return (
-        <EthParent request={request} config={config} key={reactKeyIndex} />
-      );
-    default:
-      return null;
+  case 'eth':
+    const config = getConfig()
+    reactKeyIndex++
+    return (
+      <EthParent request={request} config={config} key={reactKeyIndex} />
+    )
+  default:
+    return null
   }
-};
+}
 
-export default reducer;
+export default reducer
