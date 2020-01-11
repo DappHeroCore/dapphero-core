b84746fdcc40e9e93c7274080dab9ed734d001e6
Dennison Bertram <dennison@dennisonbertram.com>
2020-01-11 18:23:35 -0500

    add sendTransactionWrapper documentation

--- src/modules/eth/utils/contract/sendTransactionWrapper.ts
+++ src/modules/eth/utils/contract/sendTransactionWrapper.ts
@@ -2,6 +2,18 @@ import { getTxFieldInputs } from '../element/getTxFieldInputs'
 import { sendTransactionToContract } from './sendTransactionToContract'
 import { clearInputFields } from './clearInputFields'
 
+/**
+ * This function wraps the sendTransactionToContract function so that we can isolate the process of
+ * getting the transaction field inputs and initiating the transaction itself.
+ * @param requests {} this is all the request strings associated with this transaction // TODO: be clearer about this terminology
+ * @param position {} this is an index of the request string // TODO: be clear what this is supposed to be?
+ * @param method {} the currenctly requested method. // TODO: be clear about what this is
+ * @param injected {} this is web3 connection
+ * @param instance {object} this is the contract instance
+ * @param signature {string} this is the signature of the method that we are invoking.
+ * @param setTxState {function} callback function to set the state of a higher up component on the status of the transaction
+ * @param signifiers {} // TODO: what is this?
+ */
 export const sendTransactionWrapper = async (
   requests,
   position,
