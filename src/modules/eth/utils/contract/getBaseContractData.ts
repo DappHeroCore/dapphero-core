b4d917d8261bf03ddbed5cb1d2b70d85f1f8f6b8
Dennison Bertram <dennison@dennisonbertram.com>
2020-01-11 17:33:41 -0500

    add additional information to docs of getBaseContractData

--- src/modules/eth/utils/contract/getBaseContractData.ts
+++ src/modules/eth/utils/contract/getBaseContractData.ts
@@ -8,6 +8,8 @@ import { useGetMethods } from './useGetMethods'
  * @param abi {object} this is the contract abi object which is required to create a contract instance.
  * @param contractAddress {string} this is the address of a deployed contract
  * @param injectedLib {object} this comes from our web3 provider and provides access to the blockchain and creating contract instances.
+ * @returns {string, object, array} returns the method we are currently working with, the instance of the contract, and all the mthods.
+ * // TODO: clarify what method is supposed to be and how this is used.
  */
 export const getBaseContractData = (
   requestString: string[],
