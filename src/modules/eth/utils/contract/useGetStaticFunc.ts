0f909bcfb0a1e65318f190b9551e8aa2b3ed985c
Dennison Bertram <dennison@dennisonbertram.com>
2020-01-11 18:25:10 -0500

    add documentation to useGetStaticFunc

--- src/modules/eth/utils/contract/useGetStaticFunc.ts
+++ src/modules/eth/utils/contract/useGetStaticFunc.ts
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react'
 
 // retVal param needed in case of function w/ multiple possible return values
+
+/**
+ * This function is a react hook that automatically calls static view functions which have no arguements.
+ * @param instance {object} this is the web3 contract instance.
+ * @param signature {string} this is the signature of the method on the instance that we are calling.
+ * @param identifiedReturnValue // TODO: I don't know what this is supopsed to be. explain?
+ */
 function useGetStaticFunc(instance: any, signature: any, identifiedReturnValue?: string | null) {
   const [ value, setValue ] = useState(null)
 
