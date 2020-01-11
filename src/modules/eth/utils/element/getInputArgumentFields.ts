8f4682133c26754f2ca34fd7611ab16821ba0d05
Dennison Bertram <dennison@dennisonbertram.com>
2020-01-11 18:32:03 -0500

    add documentation to getInputArgumentFields

--- src/modules/eth/utils/element/getInputArgumentFields.ts
+++ src/modules/eth/utils/element/getInputArgumentFields.ts
@@ -7,6 +7,14 @@
 // Basically we take in all the elements for the module, so for example "transfer"
 // requires a transfer-amount and transfer-recipient elements.
 
+/**
+ * This function gathers all the input fields (html elements) on a page which are associated with a particular module.
+ * @param modules {array} // TODO: Clarify what is coming in. I think it is an array of modules (HTML elements)
+ * @param position {number} this should be the index in the request string we are currently accessing TODO: get clarity on what we are going to call this.
+ * @param request {string} TODO: Clarify what the purpose of this is.
+ * @param method {string} TODO: Clarify what this is.
+ * @returns two arrays, one of inputFields and one of transaction arguments.
+ */
 export const getInputArgumentFields: {inputFields: {[key: string]:string}[], txArgArray: string[]} = (modules, position, request, method) => { //eslint-disable-line
 
   const inputFields = modules.filter((module) => module.requestString[position] === request
