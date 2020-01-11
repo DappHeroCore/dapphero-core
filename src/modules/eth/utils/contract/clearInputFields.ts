49fe774e6bfa15f4ba50dbc6a4cbebc01a6df6c1
Dennison Bertram <dennison@dennisonbertram.com>
2020-01-11 17:25:16 -0500

    Add documentation to function clearInputFields

--- src/modules/eth/utils/contract/clearInputFields.ts
+++ src/modules/eth/utils/contract/clearInputFields.ts
@@ -2,6 +2,10 @@
 // TODO: the functionality for clearning input fields should be extracted to external function
 // Timeout set because function needs to pull value first
 
+/**
+ * This function clears the input HTML fields on a users website after a transaction has been initiated.
+ * @param inputFields {array} this is an array of HTML elements which represents input fields.
+ */
 export const clearInputFields = (inputFields) => {
   setTimeout(() => {
     inputFields.forEach((module) => {
