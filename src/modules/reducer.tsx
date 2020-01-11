bcb49efa0f817c87ba5efded232a1ffdae92312b
DrakeEvans <ecg.drake@gmail.com>
2020-01-10 20:09:07 -0500

    ESLint rules update

--- src/modules/reducer.tsx
+++ src/modules/reducer.tsx
@@ -16,11 +16,11 @@ const getConfig = (): DappHeroConfig => {
 // This increments a key so each element out of reducer has unique Key so react doesn't complain.
 let reactKeyIndex = 0
 
-const reducer = (request: Request) => {
+export const reducer = (request: Request) => {
   switch (request.arg) {
   case 'eth': {
     const config = getConfig()
-    reactKeyIndex++
+    reactKeyIndex += 1
     return (
       <EthParent request={request} config={config} key={reactKeyIndex} />
     )
