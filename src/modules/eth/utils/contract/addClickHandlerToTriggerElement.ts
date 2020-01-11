6033724f9c6e75184fb1e4076e24808bf41342d8
Dennison Bertram <dennison@dennisonbertram.com>
2020-01-11 17:20:23 -0500

    add documentation to callPublicMethodWithArg

--- src/modules/eth/utils/contract/addClickHandlerToTriggerElement.ts
+++ src/modules/eth/utils/contract/addClickHandlerToTriggerElement.ts
@@ -2,7 +2,7 @@
  * This function adds a click hander to an HTML element which is identified by the user
  * as a "trigger emlement". A trigger element is an element which initiates some kind of
  * action when clicked on (or interacted with). In this case it is typically a button and
- * used to allow the front end to initiate a blockchain transacstion on the user behalf. 
+ * used to allow the front end to initiate a blockchain transacstion on the user behalf.
  * @param triggerElement {HTMLElement} the target DOM element for a onClick function.
  * @param callBackFunction {function} the function which is invoked when element is clicked.
  */
