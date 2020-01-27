77f872804f1831b75ae82e8609bd638f20057006
DrakeEvans <ecg.drake@gmail.com>
2020-01-26 19:08:07 -0500

    WIP
    Co-authored-by: Dennison Bertram <dennison@dennisonbertram.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,19 @@ import React, { useEffect, useState } from 'react'
 import * as api from 'api'
 
 import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
 import { ethers } from 'ethers'
 import { Activator } from './Activator'
 
+// function getLibrary(provider) {
+//   return new ethers.providers.Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+// }
+
 function getLibrary(provider) {
-  return new ethers.providers.Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
+  const library = new Web3Provider(provider)
+  console.log('Library: ', library)
+  library.pollingInterval = 8000
+  return library
 }
 
 export const App: React.FC = () => (
