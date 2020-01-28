cf286bbc27b806b77eed22c010cc00c94203da93
Leonardo Galante <lndgalante@gmail.com>
2020-01-28 18:52:44 -0300

    Trigger method contracts

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { ToastProvider } from 'react-toast-notifications'
+
+import * as api from 'api'
 
 import { Web3ReactProvider } from '@web3-react/core'
 import { ethers } from 'ethers'
@@ -7,7 +10,9 @@ import { Activator } from './Activator'
 const getLibrary = (provider) => new ethers.providers.Web3Provider(provider) // this will vary according to whether you use e.g. ethers or web3.js
 
 export const App: React.FC = () => (
-  <Web3ReactProvider getLibrary={getLibrary}>
-    <Activator />
-  </Web3ReactProvider>
+  <ToastProvider>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Activator />
+    </Web3ReactProvider>
+  </ToastProvider>
 )
--- src/protocol/ethereum/customContract-/Reducer.tsx
+++ src/protocol/ethereum/customContract-/Reducer.tsx
@@ -1,14 +1,107 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useToasts } from 'react-toast-notifications'
+import { ethers } from 'ethers'
 
+// Hooks
+import * as hooks from 'hooks'
+
+// Utils
+const getAbiMethodInputs = (abi, methodName) => {
+  const method = abi.find(({ name }) => name === methodName)
+  return method.inputs.reduce((acc, { name }) => ({ ...acc, [name]: '' }), [])
+}
+
+// Reducer Component
 export const Reducer = ({ info }) => {
-  const { contract, childrenElements, properties, modifiers } = info
-  console.log('TCL: Reducer -> info', info)
-  console.log('TCL: Reducer -> contract', contract)
-  console.log('TCL: Reducer -> modifiers', modifiers)
-  console.log('TCL: Reducer -> properties', properties)
-  console.log('TCL: Reducer -> childrenElements', childrenElements)
+  const { contract, childrenElements, properties, isTransaction, hasParameters } = info
+  const { address, abi } = contract
 
-  const [ name, setName ] = useState('')
+  const contractKey = properties.find(({ key }) => key === 'contractName')
+  const methodKey = properties.find(({ key }) => key === 'methodName')
 
-  return <div>....</div>
+  const { value: contractName } = contractKey
+  const { value: methodName } = methodKey
+
+  // Custom Hooks
+  const { addToast } = useToasts()
+  const injectedContext = hooks.useDappHeroWeb3()
+
+  // States
+  const [ context, setcontext ] = useState(injectedContext)
+  const [ runMethod, setRunMethod ] = useState(null)
+  const [ parameters, setParameters ] = useState(getAbiMethodInputs(info.contract.abi, methodName))
+
+  // -> Handlers
+  const handleRunMethod = async () => {
+    if (!runMethod) return console.error(`No method to run yet`)
+
+    const parametersValues = Object.values(parameters)
+
+    if (hasParameters) {
+      const isParametersFilled = Boolean(parametersValues.filter(Boolean).join(''))
+      if (!isParametersFilled) return console.error(`You must define your parameters first`)
+    }
+
+    try {
+      const methodParams = [ ...(hasParameters ? parametersValues : []) ]
+
+      // TODO: Use isTransaction key to trigger transaction
+      const result = await runMethod(...methodParams)
+
+      console.log('TCL: handleRunMethod -> result', result.toString())
+
+      // TODO: Check if result is an object and check if there's an output-name with one of those key names
+      // Insert result in all output elements
+      const outputs = childrenElements.filter(({ id }) => id.includes('output'))
+      outputs.forEach(({ element }) => Object.assign(element, { textContent: result }))
+    } catch (error) {
+      console.error('TCL: handleRunMethod -> error', error)
+    }
+  }
+
+  // -> Effects
+  useEffect(() => {
+    setcontext(injectedContext)
+  }, [ injectedContext.networkId ])
+  const { lib } = context
+
+  // Create contract and new method to trigger in button
+  useEffect(() => {
+    if (lib) {
+      const contractInstance = new ethers.Contract(address, abi, lib)
+
+      if (contractInstance[methodName]) {
+        const methodToRun = () => contractInstance[methodName]
+        setRunMethod(methodToRun)
+      } else {
+        console.error(`Method "${methodName}" does not exists on contract "${contractName}"`)
+      }
+    }
+  }, [ lib ])
+
+  // Add triggers to input elements
+  useEffect(() => {
+    const inputChildrens = childrenElements.filter(({ id }) => id.includes('input'))
+
+    if (inputChildrens.length > 0) {
+      const [ inputs ] = inputChildrens
+
+      inputs.element.forEach(({ element, argumentName }) => {
+        element.addEventListener('input', ({ target: { value } }) => {
+          setParameters((prevParameters) => ({ ...prevParameters, [argumentName]: value }))
+        })
+      })
+    }
+  }, [ childrenElements ])
+
+  // Add trigger to invoke buttons
+  useEffect(() => {
+    const invokeButtons = childrenElements.filter(({ id }) => id.includes('invoke'))
+
+    if (invokeButtons) {
+      invokeButtons.forEach(({ element }) => element.addEventListener('click', handleRunMethod))
+    }
+  }, [ childrenElements, handleRunMethod ])
+
+  return null
 }
