c843f1db4c604993cc5601f2b18f725e61050eba
Dennison Bertram <dennison@dennisonbertram.com>
2020-01-28 12:43:46 -0500

    Send ETH working with toast notification

--- src/protocol/ethereum/network/EthTransfer.tsx
+++ src/protocol/ethereum/network/EthTransfer.tsx
@@ -1,7 +1,9 @@
 import { FunctionComponent, useEffect } from 'react'
+import Notify from 'bnc-notify'
 import * as hooks from 'hooks'
 import * as utils from 'utils'
 
+const apiKey = process.env.REACT_APP_BLOCKNATIVE_API
 interface EthTransferProps {
   element: HTMLElement
 }
@@ -14,10 +16,17 @@ export const EthTransfer: FunctionComponent<EthTransferProps> = ({ element }) =>
   const inputNodes = document.querySelectorAll(`[id^=dh-network-transfer]`)
   const { lib } = hooks.useDappHeroWeb3()
 
+  console.log('THE LIB COMING IN: ', lib)
+
   useEffect(() => {
     console.log('ETHTRANSFER PROVIDER', lib)
     const transferEther = (e) => {
       e.preventDefault()
+
+      const notify = Notify({
+        dappId: apiKey, // [String] The API key created by step one above
+        networkId: lib._network.chainId, // [Integer] The Ethereum network ID your Dapp uses.
+      })
 
       let toAddress
       let value
@@ -28,8 +37,16 @@ export const EthTransfer: FunctionComponent<EthTransferProps> = ({ element }) =>
         if (inputSplit[4] === 'value') value = (input as HTMLInputElement).value
         if (inputSplit[4] === 'to') toAddress = (input as HTMLInputElement).value
       })
+      console.log('The Value: ', utils.convertUnits('ether', 'wei', value).toHexString())
 
-      lib.sendTransaction({ to: toAddress, from, value: utils.convertUnits('ether', 'wei', value) }).catch((e) => console.log(e))
+      const params = [ {
+        from,
+        to: toAddress,
+        value: utils.convertUnits('ether', 'wei', value).toHexString(),
+      } ]
+
+      lib.send('eth_sendTransaction', params).then((hash) => notify.hash(hash))
+      // lib.sendTransaction({}).catch((e) => console.log(e))
     }
 
     if (lib) utils.addClickHandlerToTriggerElement(element, transferEther)
