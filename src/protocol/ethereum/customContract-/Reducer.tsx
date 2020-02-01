473f688239d13fd5c706cc623b1c60a46767e1aa
Leonardo Galante <lndgalante@gmail.com>
2020-01-31 23:07:31 -0300

    Add suport for eth values

--- src/protocol/ethereum/customContract-/Reducer.tsx
+++ src/protocol/ethereum/customContract-/Reducer.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, useCallback } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useToasts } from 'react-toast-notifications'
 import { ethers } from 'ethers'
+import omit from 'lodash.omit'
 
 // Hooks
 import * as hooks from 'hooks'
@@ -13,14 +14,13 @@ const getAbiMethodInputs = (abi, methodName) => {
 
 // Reducer Component
 export const Reducer = ({ info }) => {
-  const { contract, childrenElements, properties, hasInputs, hasOutputs, modifiers } = info
+  const { contract, childrenElements, properties, hasInputs, hasOutputs, isTransaction, modifiers } = info
   const { contractAddress, contractAbi } = contract
 
   const methodNameKey = properties.find(({ key }) => key === 'methodName')
-  const contractKey = properties.find(({ key }) => key === 'contractName')
+  const ethValueKey = properties.find((property) => property.key === 'ethValue')
 
   const { value: methodName } = methodNameKey
-  const { value: contractName } = contractKey
 
   // Custom Hooks
   hooks.useEagerConnect()
@@ -30,59 +30,55 @@ export const Reducer = ({ info }) => {
   const displayToast = ({ message }: Error) => addToast(message, { appearance: 'error' })
 
   // States
-  const [ context, setcontext ] = useState(injectedContext)
   const [ result, setResult ] = useState(null)
-  const [ runMethod, setRunMethod ] = useState(null)
-  const [ getGasLimit, setGetGasLimit ] = useState(null)
+  const [ context, setcontext ] = useState(injectedContext)
   const [ parameters, setParameters ] = useState(getAbiMethodInputs(info.contract.contractAbi, methodName))
 
   // -> Handlers
-  const handleRunMethod = useCallback(async (method, methodParametersMap) => {
-    if (!method) return console.error(`No method to run yet`)
-
-    const parametersValues = Object.values(methodParametersMap)
+  const handleRunMethod = async () => {
+    const ethValue = parameters?.EthValue
+    const parsedParameters = omit(parameters, 'EthValue')
+    const parametersValues = Object.values(parsedParameters)
 
     if (hasInputs) {
       const isParametersFilled = Boolean(parametersValues.filter(Boolean).join(''))
-      if (!isParametersFilled) return console.error(`You must define your parameters first`)
+      if (!isParametersFilled) console.error(`You must define your parameters first`)
+
     }
 
     try {
+      let value = '0'
       const methodParams = [ ...(hasInputs ? parametersValues : []) ]
+
+      // TODO: Test send eth value to method
+      if (ethValueKey || ethValue) {
+        value = ethValueKey?.value || ethValue
+      }
 
       // TODO: Get gas limit through ethers, and remove MAX_LIMIT
       // const gasLimit = await getGasLimit(...methodParams)
-      const methodResult = await method(...methodParams)
 
-      setResult(methodResult)
+      const signer = new ethers.providers.Web3Provider(window.ethereum).getSigner()
+      const contractInstance = new ethers.Contract(contractAddress, contractAbi, signer)
+
+      if (isTransaction) {
+        const options = { to: '', value: ethers.utils.parseEther(value) }
+        const tx = await signer.sendTransaction(options)
+        setResult(tx)
+      } else {
+        const method = contractInstance.functions[methodName]
+        const methodResult = await method(...methodParams)
+        setResult(methodResult)
+      }
     } catch (error) {
       displayToast(error)
     }
-  }, [ ])
+  }
 
   // -> Effects
   useEffect(() => {
     setcontext(injectedContext)
   }, [ injectedContext.networkId ])
-  const { lib } = context
-
-  // Create contract instance and set new method to trigger afterward in button
-  useEffect(() => {
-    if (lib) {
-      const signer = new ethers.providers.Web3Provider(lib.provider).getSigner()
-      const contractInstance = new ethers.Contract(contractAddress, contractAbi, signer)
-
-      if (contractInstance[methodName]) {
-        const methodToRun = () => contractInstance.functions[methodName]
-        const getterGasLimit = () => contractInstance.estimate[methodName]
-
-        setRunMethod(methodToRun)
-        setGetGasLimit(getterGasLimit)
-      } else {
-        console.error(`Method "${methodName}" does not exists on contract "${contractName}"`)
-      }
-    }
-  }, [ lib ])
 
   // Add triggers to input elements
   useEffect(() => {
@@ -93,7 +89,10 @@ export const Reducer = ({ info }) => {
 
       inputs.element.forEach(({ element, argumentName }) => {
         element.addEventListener('input', ({ target: { value } }) => {
-          setParameters((prevParameters) => ({ ...prevParameters, [argumentName]: value }))
+          setParameters((prevParameters) => ({
+            ...prevParameters,
+            [argumentName]: value,
+          }))
         })
       })
     }
@@ -104,23 +103,27 @@ export const Reducer = ({ info }) => {
     const invokeButtons = childrenElements.filter(({ id }) => id.includes('invoke'))
 
     if (invokeButtons) {
-      invokeButtons.forEach(({ element }) => element.addEventListener('click', () => handleRunMethod(runMethod, parameters)))
+      invokeButtons.forEach(({ element }) => element.addEventListener('click', handleRunMethod))
     }
-  }, [ childrenElements, runMethod, parameters ])
+
+    return () => invokeButtons.forEach(({ element }) => element.removeEventListener('click', handleRunMethod))
+  }, [ childrenElements, handleRunMethod ])
 
   // Display new results in the UI
   useEffect(() => {
     if (result) {
-    // TODO: Check output type
+      // TODO: Check output type
       let parsedValue = result
 
-      // Handle modifiers
+      // TODO: Handle modifiers
       modifiers.forEach(({ key, value }) => {
         if (key === 'units') {
-          if (value === 'ether') parsedValue = ethers.utils.formatEther(parsedValue)
+          if (value === 'ether') {
+            parsedValue = ethers.utils.formatEther(parsedValue)
+          }
         }
         if (key === 'decimals') parsedValue = Number(parsedValue).toFixed(value)
-      // if (key === 'display') {}  Handle short and full values
+        // if (key === 'display') {}  Handle short and full values
       })
 
       // TODO: Check if result is an object and check if there's an output-name with one of those key names
