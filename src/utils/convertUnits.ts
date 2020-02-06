82d92da4b3f5a0ed82891ae3f1088b95f0c2710e
DrakeEvans <ecg.drake@gmail.com>
2020-02-06 15:16:41 -0500

    remove unnecesary functions

--- src/utils/convertUnits.ts
+++ src/utils/convertUnits.ts
@@ -1,34 +1,40 @@
 import { ethers } from 'ethers'
+import { logger } from 'logger/customLogger'
 
 export const convertUnits = (inputValueType, outputValueType, value) => {
   switch (inputValueType) {
-  case ('wei'): {
-    if (outputValueType === 'ether') return ethers.utils.formatEther(value)
-    if (outputValueType === 'wei') return value
-    break
+    case ('wei'): {
+      if (outputValueType === 'ether') return ethers.utils.formatEther(value)
+      if (outputValueType === 'wei') return value
+      break
+    }
+    case ('ether'): {
+      if (outputValueType === 'wei') return ethers.utils.parseEther(value).toString()
+      if (outputValueType === 'ether') return value
+      break
+    }
+    case ('address'): {
+      if (outputValueType === 'short') return value.slice(0, 3) + '...' + value.slice(value.length - 3)
+      if (outputValueType === 'full') return value
+      break
+    }
+    case ('bytes32'): {
+      if (outputValueType === 'ascii') return ethers.utils.parseBytes32String(value)
+      if (outputValueType === 'bytes32') return value
+      break
+    }
+    case ('ascii'): {
+      if (outputValueType === 'bytes32') return ethers.utils.formatBytes32String(value)
+      if (outputValueType === 'ascii') return value
+      break
+    }
+    default: {
+      const error = new Error(`You attempted to convert units using inputType: ${inputValueType} which is not supported`)
+      logger.warn(error)
+      throw error
+    }
   }
-  case ('ether'): {
-    if (outputValueType === 'wei') return ethers.utils.parseEther(value).toString()
-    if (outputValueType === 'ether') return value
-    break
-  }
-  case ('address'): {
-    if (outputValueType === 'short') return value.slice(0, 3) + '...' + value.slice(value.length - 3)
-    if (outputValueType === 'full') return value
-    break
-  }
-  case ('bytes32'): {
-    if (outputValueType === 'ascii') return ethers.utils.parseBytes32String(value)
-    if (outputValueType === 'bytes32') return value
-    break
-  }
-  case ('ascii'): {
-    if (outputValueType === 'bytes32') return ethers.utils.formatBytes32String(value)
-    if (outputValueType === 'ascii') return value
-    break
-  }
-  default:
-    throw new Error(`You attempted to convert units using inputType: ${inputValueType} which is not supported`)
-  }
-  throw new Error(`You attempted to convert units using outputType: ${outputValueType} which is not supported`)
+  const error = new Error(`You attempted to convert units using outputType: ${outputValueType} which is not supported`)
+  logger.warn(error)
+  throw error
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,14 +1,3 @@
-export * from './useGetMethods'
-
-export * from './sendTransactionToContract'
-export * from './sanitizeTransactionArguments'
-export * from './callPublicMethodWithArgs'
 export * from './parseIdTag'
-export * from './useDecimalAndDisplayFormat'
-export * from './useDecimalFormatter'
-export * from './useDisplayFormatter'
-
-export * from './getTxFieldInputs'
-export * from './clearInputFields'
 export * from './addClickHandlerToTriggerElement'
-export { convertUnits } from './convertUnits'
+export * from './convertUnits'
