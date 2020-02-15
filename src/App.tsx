f314aa5b9c4a8ef67c7b66a9f7bf653bad8692f7
DrakeEvans <ecg.drake@gmail.com>
2020-02-15 14:04:03 -0700

    Add telegram and docs link to verbose logging warning

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import * as helpers from 'helpers'
 import { ProvidersWrapper } from './ProvidersWrapper'
 
 export const App: React.FC = () => {
-  console.info('After many requests, we have turned verbose logging on by default during ethDenver (until the 17)')
+  console.info('==========================')
+  console.info('A MESSAGE FROM DAPPHERO:\nAfter many requests, we have turned verbose logging on by default during ethDenver (until the 17)')
+  console.info('Ask for help on Telegram:\nhttps://t.me/dapphero')
+  console.info('Docs: https://docs.dapphero.io')
+  console.info('==========================')
 
   helpers.cookies.startup()
 
