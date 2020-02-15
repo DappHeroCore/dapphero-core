e92529bb065a6adb72c738e7e2992c161bd2af48
DrakeEvans <ecg.drake@gmail.com>
2020-02-15 13:42:49 -0700

    Turn on verbose logging

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import * as helpers from 'helpers'
 import { ProvidersWrapper } from './ProvidersWrapper'
-import { logger } from './logger/customLogger'
 
 export const App: React.FC = () => {
+  console.info('After many requests, we have turned verbose logging on by default during ethDenver (until the 17)')
 
   helpers.cookies.startup()
 
