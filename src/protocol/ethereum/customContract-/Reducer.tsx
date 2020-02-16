111676867fe9e6992bbefe0d33833df5067a01f4
DrakeEvans <ecg.drake@gmail.com>
2020-02-15 23:17:59 -0700

    Fork for user memeconcious

--- src/protocol/ethereum/customContract-/Reducer.tsx
+++ src/protocol/ethereum/customContract-/Reducer.tsx
@@ -122,8 +122,8 @@ export const Reducer = ({ info, configuration }) => {
           const methodResult = await method(...methodParams)
           setResult(methodResult)
         } catch (err) {
-          logger.info('Invoke contract method failed in view.  This happends when a contract is invoked on the wrong network or when a contract is not deployed on the current network\n', err)
-          infoToast({ message: 'Invoking a contract function failed.  Are you on the right network?' })
+          // logger.info('Invoke contract method failed in view.  This happends when a contract is invoked on the wrong network or when a contract is not deployed on the current network\n', err)
+          // infoToast({ message: 'Invoking a contract function failed.  Are you on the right network?' })
         }
       }
       const [ input ] = childrenElements.filter(({ id }) => id.includes('input'))
