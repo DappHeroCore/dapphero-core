858f362b7e48cedf160530310a8dc3a75506f36a
Leonardo Galante <lndgalante@gmail.com>
2020-02-18 18:56:09 -0300

    Add 2 basic tests

---
+++ src/e2e/customContract.test.ts
@@ -0,0 +1,105 @@
+import puppeteer from 'puppeteer'
+import { ethers } from 'ethers'
+import * as dappeteer from 'dappeteer'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// Helpers
+const createMethodAttribute = (id): string => `[data-dh-property-method-id="${id}"]`
+const createOutputAttribute = (name = ''): string => `[data-dh-property-outputs="${name}"]`
+const createInputAttribute = (name = ''): string => `[data-dh-property-input-name="${name}"]`
+
+const createSubmitButtonSelector = (id): string => `button${createMethodAttribute(id)}`
+const createOutputDivSelector = (id): string => `div${createMethodAttribute(id)}${createOutputAttribute()}`
+const createInputDivSelector = (id, inputName): string => `input${createMethodAttribute(id)}${createInputAttribute(inputName)}`
+
+// Constants
+const NETWORKS = { rinkeby: 'rinkeby' }
+const ACCOUNTS = { main: 1 }
+
+// Increast Jest default timeout
+jest.setTimeout(20000)
+
+describe('Test CustomContract Feature', () => {
+  let page
+  let browser
+  let metamask
+
+  beforeAll(async (done) => {
+    browser = await dappeteer.launch(puppeteer, {
+      slowMo: 20,
+      devtools: true,
+      defaultViewport: null,
+    })
+    metamask = await dappeteer.getMetamask(browser, {
+      seed: process.env.SEED,
+      password: process.env.PASSWORD,
+    })
+
+    await metamask.switchNetwork(NETWORKS.rinkeby)
+    await metamask.switchAccount(ACCOUNTS.main)
+
+    page = await browser.newPage()
+    done()
+  })
+
+  it('should test "hello" method', async () => {
+    await page.goto('http://localhost:3000/', { waitUntil: 'networkidle2' })
+
+    // Wait until React loads and run internal logic
+    await page.waitFor(4000)
+
+    const ID = '000'
+    const outputDivSelector = createOutputDivSelector(ID)
+    const submitButtonSelector = createSubmitButtonSelector(ID)
+
+    await page.click(submitButtonSelector)
+
+    // Wait until method it's triggered and returns value
+    await page.waitFor(3000)
+
+    const outputDiv = await page.$(outputDivSelector)
+    const outputText = await page.evaluate((el) => el.innerText, outputDiv)
+
+    const EXPECTED_OUTPUT = 'Howdy'
+    expect(outputText).toBe(EXPECTED_OUTPUT)
+  })
+
+  it('should test "sendEthWithArgs" method', async () => {
+    // Method id
+    const ID = '001'
+
+    // Selectors
+    const outputDivSelector = createOutputDivSelector(ID)
+    const submitButtonSelector = createSubmitButtonSelector(ID)
+    const inputDivSelector = createInputDivSelector(ID, 'simpleMessage')
+
+    // Type
+    const message = ethers.utils.formatBytes32String('DappHero')
+    console.log('TCL: message', message)
+    await page.focus(inputDivSelector)
+    await page.keyboard.type(message)
+
+    // Submit Method
+    await page.click(submitButtonSelector)
+
+    // Confirm Metamask transaction
+    await metamask.confirmTransaction()
+    await page.bringToFront()
+
+    // Wait until method it's triggered and returns value
+    await page.waitFor(3000)
+
+    const outputDiv = await page.$(outputDivSelector)
+    const outputText = await page.evaluate((el) => el.innerText, outputDiv)
+    console.log('TCL: outputText', outputText)
+
+    const isHexString = ethers.utils.isHexString(outputText)
+    expect(isHexString).toBeTruthy()
+  })
+
+  afterAll(async () => {
+    // await browser.close()
+  })
+})
--- src/protocol/ethereum/featureReducer.tsx
+++ src/protocol/ethereum/featureReducer.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useWeb3React } from '@web3-react/core'
+
 // Reducers
 import { Reducer as NetworkReducer } from './network/Reducer'
 import { Reducer as UserReducer } from './user/Reducer'
@@ -10,33 +11,48 @@ import { Reducer as CustomContractReducer } from './customContract-/Reducer'
 // Types
 import { FeatureReducerProps } from './types'
 
-export const FeatureReducer = ({ feature, element, configuration, info }: FeatureReducerProps) => {
+// Constants
+const isProduction = process.env.NODE_ENV === 'production'
 
+export const FeatureReducer = ({ feature, element, configuration, info }: FeatureReducerProps) => {
   const injectedContext = useWeb3React()
   const featureType = feature
 
+  if (info?.properties_?.methodId === '001') {
+    console.log('TCL: FeatureReducer -> info', info)
+    console.log('TCL: FeatureReducer -> featureType', featureType)
+    console.log('TCL: FeatureReducer -> injectedContext', injectedContext)
+  }
+
   switch (featureType) {
-    case 'nft':
+    case 'nft': {
       return <NftReducer element={element} configuration={configuration} info={info} />
-    case 'user':
+    }
+
+    case 'user': {
       return <UserReducer element={element} info={info} />
-    case 'network':
+    }
+
+    case 'network': {
       return <NetworkReducer element={element} info={info} />
-    case 'threebox':
+    }
+
+    case 'threebox': {
       return <ThreeBoxReducer element={element} info={info} />
+    }
+
     case 'customContract': {
       if (
-        injectedContext?.chainId
-        && info?.contract.networkId
-        && injectedContext.chainId === info.contract.networkId
+        !isProduction
+        || (injectedContext?.chainId && info?.contract.networkId && injectedContext.chainId === info.contract.networkId)
       ) {
         return <CustomContractReducer element={element} configuration={configuration} info={info} />
       }
+
       return null
     }
+
     default:
       return null
-
   }
-
 }
