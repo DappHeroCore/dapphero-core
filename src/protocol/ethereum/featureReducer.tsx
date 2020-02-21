c39b6b1746cc58a9d5197449bf05807c0936caa6
Leonardo Galante <lndgalante@gmail.com>
2020-02-21 12:40:12 -0300

    Update README. Add test:e2e script. Config lint-staged to run only when customContract changes

--- src/protocol/ethereum/featureReducer.tsx
+++ src/protocol/ethereum/featureReducer.tsx
@@ -6,7 +6,7 @@ import { Reducer as NetworkReducer } from './network/Reducer'
 import { Reducer as UserReducer } from './user/Reducer'
 import { Reducer as ThreeBoxReducer } from './threeBox/Reducer'
 import { Reducer as NftReducer } from './nft/Reducer'
-import { Reducer as CustomContractReducer } from './customContract-/Reducer'
+import { Reducer as CustomContractReducer } from './customContract/Reducer'
 
 // Types
 import { FeatureReducerProps } from './types'
