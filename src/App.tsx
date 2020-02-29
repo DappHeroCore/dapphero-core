4c690418bac9033b028586acef69e61533cf92b0
DrakeEvans <ecg.drake@gmail.com>
2020-02-29 13:03:17 -0500

    Change debug message

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { ProvidersWrapper } from './ProvidersWrapper'
 
 export const App: React.FC = () => {
   console.info('==========================')
-  console.info('A MESSAGE FROM DAPPHERO:\nAfter many requests, we have turned verbose logging on by default during ethDenver (until the 17)')
+  console.info('A MESSAGE FROM DAPPHERO')
   console.info('Ask for help on Telegram:\nhttps://t.me/dapphero')
   console.info('Docs: https://docs.dapphero.io')
   console.info('==========================')
