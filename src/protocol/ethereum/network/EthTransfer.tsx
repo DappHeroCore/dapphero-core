62cf7f306bfffe417e66c120b6f82110d20b41ab
Dennison Bertram <dennison@dennisonbertram.com>
2020-04-12 17:19:30 -0400

    ethTransferWorking

--- src/protocol/ethereum/network/EthTransfer.tsx
+++ src/protocol/ethereum/network/EthTransfer.tsx
@@ -4,7 +4,6 @@ import { logger } from 'logger/customLogger'
 import * as utils from 'utils'
 import * as contexts from 'contexts'
 import { ethers } from 'ethers'
-import { useWeb3React } from '@web3-react/core'
 
 const apiKey = process.env.REACT_APP_BLOCKNATIVE_API
 interface EthTransferProps {
@@ -17,8 +16,6 @@ interface EthTransferProps {
 }
 
 export const EthTransfer: FunctionComponent<EthTransferProps> = ({ element, amountObj, addressObj, outputObj, info }) => {
-  const { library } = useWeb3React()
-
   const ethereum = useContext(contexts.EthereumContext)
   const { chainId, signer, writeProvider } = ethereum
 
@@ -35,10 +32,8 @@ export const EthTransfer: FunctionComponent<EthTransferProps> = ({ element, amou
         })
 
         const from = await signer.getAddress()
-        console.log('transferEther -> from', from)
         const inputUnits = amountObj?.modifiers_?.displayUnits ?? 'wei' // FIXME: move this to dappheroDOM
         const convertedUnits = utils.convertUnits(inputUnits, 'wei', amountObj.element.value)
-        console.log('transferEther -> convertedUnits', convertedUnits)
         const params = [ {
           from,
           to: addressObj.element.value,
@@ -46,12 +41,7 @@ export const EthTransfer: FunctionComponent<EthTransferProps> = ({ element, amou
           // value: utils.convertUnits(inputUnits, 'wei', amountObj.element.value),
         } ]
 
-        const { ethereum: ethereum2 } = window
-        // Request account access if needed
-        await ethereum2.enable()
-        const provider = new ethers.providers.Web3Provider(ethereum2)
-
-        provider.sendTransaction('eth_sendTransaction', params)
+        writeProvider.send('eth_sendTransaction', params)
           .then((hash) => {
             notify.hash(hash)
             amountObj.element.value = ''
@@ -60,11 +50,9 @@ export const EthTransfer: FunctionComponent<EthTransferProps> = ({ element, amou
           .catch((err) => {
             amountObj.element.value = ''
             addressObj.element.value = ''
-            console.log('THE SEND ERROR', err)
             logger.info('There was an error sending ether with metaMask', err)
           })
       } catch (err) {
-        console.log('What is the error: ', err)
         logger.warn('There was an error transfering ether', err)
       }
     }
