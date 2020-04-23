1c4e0f8a1f08e48be3fb9ec9040e6201bee2cd20
Dennison Bertram <dennison@dennisonbertram.com>
2020-04-23 19:29:14 -0400

    👌 IMPROVE: Delete old unused code

--- src/connectors/injected.ts
+++
@@ -1,3 +0,0 @@
-import { InjectedConnector } from '@web3-react/injected-connector'
-// TODO: [DEV-118] pull supported chains from config or consts file
-export const injectedConnector = new InjectedConnector({ supportedChainIds: [ 1, 3, 4, 5, 10, 42, 77, 99, 100 ] })
