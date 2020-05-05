5efedd1cc0efb13df76db72525128cd2f064e6e6
Dennison Bertram <dennison@dennisonbertram.com>
2020-05-05 12:39:45 -0400

    👌 IMPROVE:  Add Warning message in console

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { ProvidersWrapper } from './ProvidersWrapper'
 export const App: React.FC = () => {
   console.info('==========================')
   console.info('A MESSAGE FROM DAPPHERO')
+  console.info('This software is currently in ALPHA, please be careful.')
+  console.info('There is no warranty, everything is at your own risk.')
   console.info('Ask for help on Telegram:\nhttps://t.me/dapphero')
   console.info('Docs: https://docs.dapphero.io')
   console.info('==========================')
