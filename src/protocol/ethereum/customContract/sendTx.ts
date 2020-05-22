2f005e595c96757bde30750139fd06b5bb995f4a
Dennison Bertram <dennison@dennisonbertram.com>
2020-05-22 18:28:07 -0400

    🐛 FIX:  Error Messages surfaced to User.

--- src/protocol/ethereum/customContract/sendTx.ts
+++ src/protocol/ethereum/customContract/sendTx.ts
@@ -12,9 +12,6 @@ export const sendTx = async ({ writeContract, dispatch, provider, methodName, me
   let estimatedGas
 
   const tempOverride = { value: ethers.utils.parseEther(value) }
-  // TODO: Allow users to set Gas Price
-  console.log('value', value)
-  // console.log("tempOverride", tempOverride)
 
   dispatch({
     type: ACTION_TYPES.txUserSignatureRequested,
@@ -27,138 +24,159 @@ export const sendTx = async ({ writeContract, dispatch, provider, methodName, me
     },
   })
 
-  // CallStatic first
-  let callStatic
-  try {
-    console.log('Calling Static')
-    callStatic = await writeContract.callStatic[methodName](...methodParams, tempOverride)
-    console.log('callStatic result', callStatic)
-  } catch (error) {
-    console.log('Error Reason:', error.reason)
-
-  }
+  // Call Static to see if it's going to Fail and get Error Message
+  let transactionValid = true
 
   try {
-    estimatedGas = await estimateMethod(...methodParams, tempOverride)
-    // const valueStatic = await method(...methodParams, tempOverride)
-    // console.log('valueStatic', valueStatic)
-
-    dispatch({
-      type: ACTION_TYPES.txUserSignatureRequested,
-      status: {
-        ...methodDetails,
-        msg: 'Estimate Gas Cost of Transaction Succedded',
-        error: false,
-        fetching: true,
-        inFlight: false,
-        estimatedGas: estimatedGas.toString(),
-      },
-    })
-
+    if (value !== '0') {
+      await writeContract.callStatic[methodName](...methodParams, tempOverride)
+    } else {
+      await writeContract.callStatic[methodName](...methodParams)
+    }
   } catch (error) {
-    console.log('Estimate gas error', error)
-    dispatch({
-      type: ACTION_TYPES.estimateGasError,
-      status: {
-        ...methodDetails,
-        msg: 'Estimate Gas Cost of Transaction Failed',
-        error,
-        fetching: false,
-        inFlight: false,
-      },
-    } )
-  }
-
-  const overrides = {
-    gasLimit: estimatedGas,
-    gasPrice,
-    value: ethers.utils.parseEther(value),
-  }
-  let methodResult
-
-  dispatch({
-    type: ACTION_TYPES.txUserSignatureRequested,
-    status: {
-      ...methodDetails,
-      msg: 'User signature requsted',
-      error: false,
-      fetching: true,
-      inFlight: false,
-    },
-  })
-
-  try {
-    // console.log('MethodNAme: ', methodName)
-    // console.log('contract: ', writeContract.functions)
-    // const method = writeContract.functions[methodName]
-    console.log('The Overrides: ', overrides)
-    console.log('The methodParams: ', methodParams)
-    methodResult = await writeContract.functions[methodName](...methodParams, overrides)
-    console.log('methodParams', methodParams)
-
-    dispatch({
-      type: ACTION_TYPES.txReceipt,
-      status: {
-        msg: `TX Broadcast.`,
-        error: false,
-        fetching: true,
-        inFlight: true,
-        txReceipt: methodResult.hash,
-        ...methodDetails,
-      },
-    })
-
-    provider.once(methodResult.hash, (receipt) => {
-      dispatch({
-        type: ACTION_TYPES.confirmed,
-        status: {
-          ...methodDetails,
-          msg: `Transaction confirmed. Hash: ${methodResult.hash}`,
-          error: false,
-          fetching: false,
-          inFlight: false,
-          receipt,
-        },
-      })
-    })
-
-    // // BlockNative Toaster to track tx
-    const { emitter } = notify.hash(methodResult.hash)
-
-    emitter.on('txSent', (data) => {
-      emitToEvent(
-        EVENT_NAMES.contract.statusChange,
-        { value: data, step: 'Transaction has been sent to the network', status: EVENT_STATUS.pending, methodNameKey },
-      )
-    })
-    emitter.on('txFailed', (data) => emitToEvent(
-      EVENT_NAMES.contract.statusChange,
-      { value: data, step: 'Transaction sent to network but failed to be mined.', status: EVENT_STATUS.rejected, methodNameKey },
-    ) )
-    emitter.on('txConfirmed', (data) => emitToEvent(
-      EVENT_NAMES.contract.statusChange,
-      { value: data, step: 'Transaction has been sent to the network', status: EVENT_STATUS.resolved, methodNameKey },
-    ) )
-
-    // Set Result on State
-    return methodResult.hash
-
-  } catch (error) {
-    console.log(Object.keys(error))
-    console.log(JSON.stringify(error, null, 2))
     emitToEvent(
       EVENT_NAMES.contract.statusChange,
-      { value: error, step: 'Transaction failed to be broadcast/executed', status: EVENT_STATUS.rejected, methodNameKey },
+      { value: error, step: `Tx will revert. Message: ${error.reason}`, status: EVENT_STATUS.rejected, methodNameKey },
     )
     dispatch({
       type: ACTION_TYPES.txError,
       status: {
         ...methodDetails,
-        msg: 'Transaction failed. Check console for more details.',
+        msg: `Tx will revert. Message:  ${error.reason}`,
         error,
         inFlight: false,
         fetching: false,
       },
-    } )
+    })
+    transactionValid = false
   }
+
+  // If the Contract would revert, block execution
+  if (transactionValid) {
+    // Estimate Gas for Transaction
+    try {
+      if (value !== '0') {
+        estimatedGas = await estimateMethod(...methodParams, tempOverride)
+      } else {
+        estimatedGas = await estimateMethod(...methodParams)
+
+      }
+
+      dispatch({
+        type: ACTION_TYPES.txUserSignatureRequested,
+        status: {
+          ...methodDetails,
+          msg: 'Estimate Gas Cost of Transaction Succedded',
+          error: false,
+          fetching: true,
+          inFlight: false,
+          estimatedGas: estimatedGas.toString(),
+        },
+      })
+
+    } catch (error) {
+      dispatch({
+        type: ACTION_TYPES.estimateGasError,
+        status: {
+          ...methodDetails,
+          msg: 'Estimate Gas Cost of Transaction Failed',
+          error,
+          fetching: false,
+          inFlight: false,
+        },
+      })
+    }
+
+    // Send the Transaction
+    try {
+      const overrides = {
+        gasLimit: estimatedGas,
+        gasPrice,
+        value: ethers.utils.parseEther(value),
+      }
+      let methodResult
+
+      dispatch({
+        type: ACTION_TYPES.txUserSignatureRequested,
+        status: {
+          ...methodDetails,
+          msg: 'User signature requsted',
+          error: false,
+          fetching: true,
+          inFlight: false,
+        },
+      })
+
+      if (value !== '0') {
+        methodResult = await writeContract.functions[methodName](...methodParams, overrides)
+      } else {
+        methodResult = await writeContract.functions[methodName](...methodParams)
+      }
+
+      console.log('methodParams', methodParams)
+
+      dispatch({
+        type: ACTION_TYPES.txReceipt,
+        status: {
+          msg: `TX Broadcast.`,
+          error: false,
+          fetching: true,
+          inFlight: true,
+          txReceipt: methodResult.hash,
+          ...methodDetails,
+        },
+      })
+
+      provider.once(methodResult.hash, (receipt) => {
+        dispatch({
+          type: ACTION_TYPES.confirmed,
+          status: {
+            ...methodDetails,
+            msg: `Transaction confirmed. Hash: ${methodResult.hash}`,
+            error: false,
+            fetching: false,
+            inFlight: false,
+            receipt,
+          },
+        })
+      })
+
+      // // BlockNative Toaster to track tx
+      const { emitter } = notify.hash(methodResult.hash)
+
+      emitter.on('txSent', (data) => {
+        emitToEvent(
+          EVENT_NAMES.contract.statusChange,
+          { value: data, step: 'Transaction has been sent to the network', status: EVENT_STATUS.pending, methodNameKey },
+        )
+      })
+      emitter.on('txFailed', (data) => emitToEvent(
+        EVENT_NAMES.contract.statusChange,
+        { value: data, step: 'Transaction sent to network but failed to be mined.', status: EVENT_STATUS.rejected, methodNameKey },
+      ))
+      emitter.on('txConfirmed', (data) => emitToEvent(
+        EVENT_NAMES.contract.statusChange,
+        { value: data, step: 'Transaction has been sent to the network', status: EVENT_STATUS.resolved, methodNameKey },
+      ))
+      // Set Result on State
+      return methodResult.hash
+
+    } catch (error) {
+      emitToEvent(
+        EVENT_NAMES.contract.statusChange,
+        { value: error, step: 'Transaction failed to be broadcast/executed', status: EVENT_STATUS.rejected, methodNameKey },
+      )
+      dispatch({
+        type: ACTION_TYPES.txError,
+        status: {
+          ...methodDetails,
+          msg: 'Transaction failed. Check console for more details.',
+          error,
+          inFlight: false,
+          fetching: false,
+        },
+      })
+    }
+  }
+
 }
