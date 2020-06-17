6c4c86fe4c3fddbbf4ccac71134d7034beef23f4
Dennison Bertram <dennison@dennisonbertram.com>
2020-06-17 13:01:35 -0400

    📦 NEW:  add more coverage for NFT events.

--- src/protocol/ethereum/nft/useGetTokensFromOwner.tsx
+++ src/protocol/ethereum/nft/useGetTokensFromOwner.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useContext } from 'react'
+import { EmitterContext } from 'providers/EmitterProvider/context'
+import { EVENT_NAMES, EVENT_STATUS } from 'providers/EmitterProvider/constants'
 import { openSeaApi } from './api'
 
 export const useGetTokensFromOwner = ({ assetOwnerAddress, isAllTokens, parsedTokens, isSingleToken, isMultipleTokens, assetContractAddress, tokens, limit, offset }) => {
   const [ nfts, setNfts ] = useState(null)
   const [ error, setError ] = useState(null)
+
+  const { actions: { emitToEvent } } = useContext(EmitterContext)
 
   useEffect(() => {
     if (!assetOwnerAddress) return
@@ -15,7 +19,13 @@ export const useGetTokensFromOwner = ({ assetOwnerAddress, isAllTokens, parsedTo
 
       openSeaApi.owner
         .getSingleAsset({ assetOwnerAddress, assetContractAddress, token })
-        .then(setNfts)
+        .then((nfts) => {
+          emitToEvent(
+            EVENT_NAMES.nft.loadSingleToken,
+            { value: nfts, step: 'Load Single Token', status: EVENT_STATUS.resolved },
+          )
+          setNfts(nfts)
+        })
         .catch((error) => setError({ simpleErrorMessage, completeErrorMessage, error }))
     }
 
@@ -25,7 +35,13 @@ export const useGetTokensFromOwner = ({ assetOwnerAddress, isAllTokens, parsedTo
 
       openSeaApi.owner
         .getMultipleAssets({ assetOwnerAddress, assetContractAddress, tokens, limit, offset })
-        .then(setNfts)
+        .then((nfts) => {
+          emitToEvent(
+            EVENT_NAMES.nft.loadMultipleTokens,
+            { value: nfts, step: 'Load Single Token', status: EVENT_STATUS.resolved },
+          )
+          setNfts(nfts)
+        })
         .catch((error) => setError({ simpleErrorMessage, completeErrorMessage, error }))
     }
 
@@ -35,7 +51,13 @@ export const useGetTokensFromOwner = ({ assetOwnerAddress, isAllTokens, parsedTo
 
       openSeaApi.owner
         .getAllAssets({ assetOwnerAddress, assetContractAddress, limit, offset })
-        .then(setNfts)
+        .then((nfts) => {
+          emitToEvent(
+            EVENT_NAMES.nft.loadAllTokens,
+            { value: nfts, step: 'Load Single Token', status: EVENT_STATUS.resolved },
+          )
+          setNfts(nfts)
+        })
         .catch((error) => setError({ simpleErrorMessage, completeErrorMessage, error }))
     }
   }, [ assetOwnerAddress, offset ])
